a9015b2599a20630dbe426f22f087ae6d97b716d
Ngdinhkhiem2 <kamisama29422@gmail.com>
2021-11-07 06:15:53 +0700

    nộp bài

---
+++ src/com/khiemnd2008110301/tuan7/Node.java
@@ -0,0 +1,20 @@
+package tuan7;
+
+public class Node {
+    
+        int data; 
+        Node next;
+       
+        public Node(){}
+    
+        public Node(int d){
+            data = d;
+    
+        }
+    
+        public Node(int d, Node n){
+            data = d;
+            next = n;
+        }
+    }
+    
